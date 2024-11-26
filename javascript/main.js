91843ebca669821d0f73230ee0e734cf36bbf7d4
cdenzelcoleman <camdcole@gmail.com>
2024-11-26 09:48:00 -0600

    Blackjack commmit

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+
+
+  /*----- state variables -----*/
+let gameState = {
+    dec: [],
+    playerHand: [],
+    playerScore: 0,
+    dealerScore: 0
+    gamePhase: 'start!', 
+    outcome: null
+
+};
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
